00c556aca8e502b65b9df983e79df804ca98471e
John Kitchin <jkitchin@andrew.cmu.edu>
2016-02-02 19:31:58 -0500

    add recipe

---
+++ kitchingroup-55.el
@@ -0,0 +1,43 @@
+;;; kitchingroup-55.el --- CaPPA preprint for doi:
+
+;; Copyright (C) 2016 John Kitchin
+
+;; Version: 0.0.1
+;; Author: John D. Michael
+;;  Ethan L. Demeter
+;;  Steven M. Illes
+;;  Qingqi Fan
+;;  Jacob R. Boes
+;;  John Kitchin <jkitchin@andrew.cmu.edu>
+;; Keywords:
+;; DOI: 10.1021/acs.jpcc.5b02458
+;; Journal: J. Phys. Chem. C
+;; Bibtex: @article{michael-2015-alkal-elect,
+;;   author =	 {John D. Michael and Ethan L. Demeter and Steven M. Illes and
+;;                   Qingqi Fan and Jacob R. Boes and John R. Kitchin},
+;;   title =	 {Alkaline Electrolyte and Fe Impurity Effects on the
+;;                   Performance and Active-Phase Structure of {NiOOH} Thin Films
+;;                   for {OER} Catalysis Applications},
+;;   journal =	 {J. Phys. Chem. C},
+;;   volume =	 {119},
+;;   number =	 {21},
+;;   pages =	 {11475-11481},
+;;   year =	 2015,
+;;   doi =		 {10.1021/acs.jpcc.5b02458},
+;;   url =		 {http://dx.doi.org/10.1021/acs.jpcc.5b02458},
+;;   keywords =	 {org-mode, DESC0004031, early-career},
+;;   date_added =	 {Fri May 15 11:25:58 2015},
+;; }
+
+;;; Commentary:
+
+;; Put some descriptive text here.
+
+;;; Code:
+(require 'cappa)
+
+(cappa-register 'kitchingroup-55)
+
+
+(provide 'kitchingroup-55)
+;;; kitchingroup-55.el ends here
